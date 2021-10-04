3d6ef9aaad9d998135f7cc476d7aaa8392b5d7f5
Cristian Nieto <cristianandresnieto@gmail.com>
2021-10-04 13:19:25 +0200

    4 - Deploying to Heroku Settings

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -17,65 +17,75 @@ Sequelize.useCLS(namespace);
 debug(myConfig.dialect);
 const sequelize = new Sequelize(myConfig);
 
-const GameModel = sequelize.define<GameInstance>("game", {
-  idgame: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const GameModel = sequelize.define<GameInstance>(
+  "game",
+  {
+    idgame: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    idplayer: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    rows: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+    columns: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+    score: {
+      type: DataTypes.NUMBER,
+    },
+    numberofmines: {
+      type: DataTypes.NUMBER,
+    },
+    board: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+    startdate: {
+      type: DataTypes.DATE,
+    },
+    pausedate: {
+      type: DataTypes.DATE,
+    },
+    enddate: {
+      type: DataTypes.DATE,
+    },
   },
-  idplayer: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  rows: {
-    type: DataTypes.NUMBER,
-    allowNull: false,
-  },
-  columns: {
-    type: DataTypes.NUMBER,
-    allowNull: false,
-  },
-  score: {
-    type: DataTypes.NUMBER,
-  },
-  numberofmines: {
-    type: DataTypes.NUMBER,
-  },
-  board: {
-    type: DataTypes.JSON,
-    allowNull: false,
-  },
-  startdate: {
-    type: DataTypes.DATE,
-  },
-  pausedate: {
-    type: DataTypes.DATE,
-  },
-  enddate: {
-    type: DataTypes.DATE,
-  },
-});
+  {
+    tableName: "games",
+  }
+);
 
-const PlayerModel = sequelize.define<PlayerInstance>("player", {
-  idplayer: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const PlayerModel = sequelize.define<PlayerInstance>(
+  "player",
+  {
+    idplayer: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    nickname: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    uid: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  nickname: {
-    type: DataTypes.STRING,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  uid: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-}, {
-  tableName: "player"
-});
+  {
+    tableName: "player",
+  }
+);
 
 GameModel.belongsTo(PlayerModel, {
   foreignKey: "idplayer",
